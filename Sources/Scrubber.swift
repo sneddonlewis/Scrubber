102351d04c62f46ef4f2ec30752df2ae0083272e
Lewis Sneddon <61014463+sneddonlewis@users.noreply.github.com>
2025-01-10 12:55:53 +0000

    passed workspace as optional path

--- Sources/Scrubber.swift
+++ Sources/Scrubber.swift
@@ -2,8 +2,8 @@ import ArgumentParser
 
 @main
 struct Scrubber : ParsableCommand {
-    @Option(help: "Specify the path to your workspace")
-    public var path: String
+    @Argument(help: "Optionally specify the path to your workspace. Defaults to the current working directory.")
+    var path: String = "./"
 
     public func run() throws {
         print("Scrub-a-dub-dub")

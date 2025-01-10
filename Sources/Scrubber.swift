d7fe1f67443fe2f0d2a2dd81d86e819597fa3ee3
Lewis Sneddon <61014463+sneddonlewis@users.noreply.github.com>
2025-01-10 13:32:55 +0000

    enumerate directory and print the paths of all sub node_modules

--- Sources/Scrubber.swift
+++ Sources/Scrubber.swift
@@ -1,3 +1,4 @@
+import Foundation
 import ArgumentParser
 
 @main
@@ -7,6 +8,32 @@ struct Scrubber : ParsableCommand {
 
     public func run() throws {
         print("Scrub-a-dub-dub")
-        print(path)
+
+        let directoryURL = URL(fileURLWithPath: path)
+        print(directoryURL)
+
+        var directoryPaths: [String] = []
+
+        if let enumerator = FileManager.default.enumerator(at: directoryURL, includingPropertiesForKeys: [.isDirectoryKey]) {
+                for case let fileURL as URL in enumerator {
+                    do {
+                        let resourceValues = try fileURL.resourceValues(forKeys: [.isDirectoryKey])
+                        if let isDirectory = resourceValues.isDirectory {
+                            if isDirectory && fileURL.path.hasSuffix("node_modules") {
+                                directoryPaths.append(fileURL.path)
+                                enumerator.skipDescendants()
+                                continue
+                            }                        }
+                    } catch {
+                        print("Error reading resource values for \(fileURL): \(error)")
+                    }
+                }
+            } else {
+                print("Could not create enumerator for directory: \(directoryURL.path)")
+            }
+
+        for dirPath in directoryPaths {
+            print(dirPath)
+        }
     }
 }

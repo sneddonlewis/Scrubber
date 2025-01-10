dd856af37bacb72ab661bcf0e48ccaadbcfe9892
Lewis Sneddon <61014463+sneddonlewis@users.noreply.github.com>
2025-01-10 13:00:41 +0000

    nah, used workspace path as flag so can be configured later

--- Sources/Scrubber.swift
+++ Sources/Scrubber.swift
@@ -2,7 +2,7 @@ import ArgumentParser
 
 @main
 struct Scrubber : ParsableCommand {
-    @Argument(help: "Optionally specify the path to your workspace. Defaults to the current working directory.")
+    @Option(help: "Optionally specify the path to your workspace. Defaults to the current working directory.")
     var path: String = "./"
 
     public func run() throws {
